edf22e8c391d0b64a4b9f6b777b7e68c42764e71
OlesiaVysotskaia <olesiavysorskaia@gmail.com>
2021-02-14 05:40:10 +0300

    add text and animation

---
+++ javascript/animation.js
@@ -0,0 +1,9 @@
+let targetsExample01 = anime({
+    targets: '.image',
+    height: ['20%', '25%'], 
+    easing: 'easeInOutBack',
+    direction: 'alternate',
+    duration: 500,
+    loop: true,
+    autoplay: true
+  })
