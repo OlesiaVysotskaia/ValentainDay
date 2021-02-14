7472468af354edb941e5fe30d092dd75b307ec55
OlesiaVysotskaia <olesiavysorskaia@gmail.com>
2021-02-15 02:43:30 +0300

    add simple animation

--- javascript/animation.js
+++ javascript/animation.js
@@ -1,9 +1,9 @@
 let targetsExample01 = anime({
     targets: '.image',
-    height: ['20%', '25%'], 
+    height: ['20%', '22%'], 
     easing: 'easeInOutBack',
     direction: 'alternate',
-    duration: 500,
+    duration: 600,
     loop: true,
     autoplay: true
   })
